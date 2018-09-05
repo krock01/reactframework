05b16e9c7ed54355e735654bf8f1d13f293d5083
krock <jack2569@163.com>
2018-09-05 09:04:36 +0800

    完成基本的井字游戏,未添加历史记录回退

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,11 @@
 import React,{Component} from 'react';
-import HelloMessage from './hello';
+import Game from './components/game';
 
 export default class App extends Component{
   render(){
     return (
       <div>
-        <HelloMessage name="krock" />
-        <HelloMessage name="jack" />
+        <Game />
       </div>
     )
   }
