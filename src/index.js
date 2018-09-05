40aa6506327593475e3dde3b2bc9e6902aa665e1
krock <jack2569@163.com>
2018-09-05 08:05:04 +0800

    删除jsx相关代码，只保留webpack配置

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './style.css';
-import App from './app';
-import React from 'react';
-import ReactDOM from 'react-dom';
 
 if(module.hot){
   module.hot.accept();
 }
-ReactDOM.render(<App />,document.getElementById('root'));
+
 
